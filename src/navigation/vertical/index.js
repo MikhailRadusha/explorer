4400d1bc89b1d004734a38e2c900dc9e8e5a69b1
Mikhail <87991370+MikhailRadusha@users.noreply.github.com>
2022-09-25 17:11:29 +0300

    Update index.js

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -19,7 +19,7 @@ function processMenu() {
       title: 'blockchains',
       children: blockchains,
       tag: `${blockchains.length}`,
-      icon: 'https://ping.pub/logo.svg',
+      icon: 'https://github.com/MikhailRadusha/explorer/blob/master/public/logo.svg',
     })
   }
   chainMenus.push({ header: 'LINKS' })
